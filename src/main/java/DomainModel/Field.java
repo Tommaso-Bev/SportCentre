f17c8e966fbfe9ad6324d91fd0a80876d39a1e1d
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2023-10-14 11:24:12 +0200

    Field and SportsCentre done

--- src/main/java/DomainModel/Field.java
+++ src/main/java/DomainModel/Field.java
@@ -1,4 +1,10 @@
 package main.java.DomainModel;
 
 public class Field {
+    private int ID;
+    private String sport;
+    private int minimumPeopleRequired;
+    private int maximumPeopleRequired;
+    private int fineph;
+    private boolean availability;
 }
--- src/main/java/DomainModel/SportsCentre.java
+++ src/main/java/DomainModel/SportsCentre.java
@@ -1,4 +1,9 @@
 package main.java.DomainModel;
 
 public class SportsCentre {
+    private int ID;
+    private String name;
+    private String address;
+    private String CAP;
+    private String type;
 }
