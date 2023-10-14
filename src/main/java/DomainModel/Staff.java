08da41bafe8817f7884f6baaf22d11c2f27dcfc8
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2023-10-14 11:55:25 +0200

    modified Staff

--- src/main/java/DomainModel/Staff.java
+++ src/main/java/DomainModel/Staff.java
@@ -6,4 +6,8 @@ public class Staff extends Person {
     private LocalDate hiringDate;
     private String task;
     private int salary;
+
+    public Staff(int ID, String fiscalCod, String name, String surname) {
+        super(ID, fiscalCod, name, surname);
+    }
 }
