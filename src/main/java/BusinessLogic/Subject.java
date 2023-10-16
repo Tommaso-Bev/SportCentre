24e5d354e6039215ee4b1c9fbe6bfff881d47b1e
Tommaso-Bev <tommaso.bevilacqua@stud.unifi.it>
2023-10-16 12:24:13 +0200

    DAO interface done

--- src/main/java/BusinessLogic/Subject.java
+++ src/main/java/BusinessLogic/Subject.java
@@ -1,4 +1,5 @@
 package main.java.BusinessLogic;
 
-public class Subject {
+public interface Subject {
+
 }
--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -1,4 +1,16 @@
 package main.java.DAO;
+import java.util.List;
 
-public class DAO {
+public interface DAO<T> {
+    public T get(int id);
+
+    public List<T> getAll();
+
+    public void save(T t);
+
+    public void modify(T t, T[] args);
+
+    public void remove(T t);
+
+    public int assignID();
 }
