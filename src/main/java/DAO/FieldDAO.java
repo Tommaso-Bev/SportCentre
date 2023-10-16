c5f2495c67f3c08c2658409b261f86e39c846dcb
Tommaso-Bev <tommaso.bevilacqua@stud.unifi.it>
2023-10-16 18:37:31 +0200

    FieldDAO get done

--- src/main/java/DAO/FieldDAO.java
+++ src/main/java/DAO/FieldDAO.java
@@ -1,10 +1,7 @@
 package main.java.DAO;
 import main.java.DomainModel.Field;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class FieldDAO implements DAO<Field>{
@@ -12,7 +9,16 @@ public class FieldDAO implements DAO<Field>{
     public Field get(int id) throws SQLException {
         Connection connection= DriverManager.getConnection("jdbc:sqlite:"+"sportCentre.db");
         Field field=null;
-        PreparedStatement prepStat=connection.prepareStatement("SELECT * FROM fields WHERE id= ?");
+        PreparedStatement prepStat=connection.prepareStatement("SELECT * FROM fields WHERE ID= ?");
+        prepStat.setInt(1, id);
+        ResultSet resSet=prepStat.executeQuery();
+        if (resSet.next()){
+            field= new Field(resSet.getInt(id), resSet.getString("sport"), resSet.getInt("minimumPeopleRequired"), resSet.getInt("maximumPeopleRequired"), resSet.getInt("fineph"), resSet.getBoolean("availability"));
+        }
+        resSet.close();
+        prepStat.close();
+        connection.close();
+        return field;
     }
 
     @Override
--- src/main/java/DomainModel/Field.java
+++ src/main/java/DomainModel/Field.java
@@ -1,10 +1,26 @@
 package main.java.DomainModel;
 
 public class Field {
+    public Field(int ID, String sport, int minimumPeopleRequired, int maximumPeopleRequired, int fineph, boolean availability) {
+        this.ID = ID;
+        this.sport = sport;
+        this.minimumPeopleRequired = minimumPeopleRequired;
+        this.maximumPeopleRequired = maximumPeopleRequired;
+        this.fineph = fineph;
+        this.availability = availability;
+    }
+
+    public Field (int ID){
+        this.ID=ID;
+    }
+
     private int ID;
     private String sport;
     private int minimumPeopleRequired;
     private int maximumPeopleRequired;
     private int fineph;
+
+
+
     private boolean availability;
 }
