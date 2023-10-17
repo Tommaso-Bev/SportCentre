8f7f933795670817a8e262bea9039262d1ef2cba
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2023-10-18 01:12:32 +0200

    fields done

--- src/main/java/DAO/FieldDAO.java
+++ src/main/java/DAO/FieldDAO.java
@@ -1,20 +1,20 @@
 package main.java.DAO;
 import main.java.DomainModel.Field;
+import main.java.DomainModel.SportsCentre;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FieldDAO implements DAO<Field>{
     @Override
     public Field get(int id) throws SQLException {
-        Connection connection= DriverManager.getConnection("jdbc:sqlite:"+"sportCentre.db");
+        Connection connection= DriverManager.getConnection("jdbc:sqlite:"+"sportCentre.sqlite");
         Field field=null;
         PreparedStatement prepStat=connection.prepareStatement("SELECT * FROM fields WHERE ID= ?");
         prepStat.setInt(1, id);
         ResultSet resSet=prepStat.executeQuery();
         if (resSet.next()){
-            field= new Field(resSet.getInt(id), resSet.getString("sport"), resSet.getInt("minimumPeopleRequired"), resSet.getInt("maximumPeopleRequired"), resSet.getInt("fineph"), resSet.getBoolean("availability"));
+            field= new Field(id, resSet.getString("sport"), resSet.getInt("minimumPeopleRequired"), resSet.getInt("maximumPeopleRequired"), resSet.getInt("fineph"), resSet.getBoolean("availability"),resSet.getInt("sportCentre") );
         }
         resSet.close();
         prepStat.close();
@@ -23,24 +23,82 @@ public class FieldDAO implements DAO<Field>{
     }
 
     @Override
-    public ArrayList<Field> getAll() {
-        return null;
+    public ArrayList<Field> getAll() throws SQLException {
+        Connection connection= DriverManager.getConnection("jdbc:sqlite:" + "sportCentre.sqlite");
+        ArrayList<Field> fields = new ArrayList<>();
+        PreparedStatement ps = connection.prepareStatement("SELECT * FROM fields");
+        ResultSet rs = ps.executeQuery();
+        while(rs.next())
+        {
+            fields.add(new Field(rs.getInt("ID"),rs.getString("sport"),rs.getInt("minimumPeopleRequired"),rs.getInt("maximumPeopleRequired"),rs.getInt("fineph"),rs.getBoolean("availability"),rs.getInt("sportCentre")));
+        }
+        rs.close();
+        ps.close();
+        connection.close();
+        return fields;
+    }
+
+
+    @Override
+    public void save(Field field) throws SQLException {
+        Connection connection= DriverManager.getConnection("jdbc:sqlite:" + "sportCentre.sqlite");
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO fields (ID, sport, minimumPeopleRequired,maximumPeopleRequired, fineph, availability, sportCentre) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        ps.setInt(1,getNextId());
+        ps.setString(2, field.getSport());
+        ps.setInt(3, field.getMinimumPeopleRequired());
+        ps.setInt(4, field.getMaximumPeopleRequired());
+        ps.setInt(5, field.getFineph());
+        ps.setBoolean(6, field.getAvailability());
+        ps.setInt(7, field.getSportCentre().getId());
+        ps.executeUpdate();
+        ps.close();
+        connection.close();
+    }
+
+
+    @Override
+    public void modify(Field field, String[] args) throws SQLException {
+        Connection connection= DriverManager.getConnection("jdbc:sqlite:" + "sportCentre.sqlite");
+        PreparedStatement ps = connection.prepareStatement("UPDATE fields SET sport = ?, minimumPeopleRequired = ?, maximumPeopleRequired = ?,fineph = ?,availability = ?   WHERE ID = ?");
+        ps.setString(1, args[0]);
+        ps.setInt(2, Integer.parseInt(args[1]));
+        ps.setInt(3, Integer.parseInt(args[2]));
+        ps.setInt(4, Integer.parseInt(args[3]));
+        ps.setInt(5, Integer.parseInt(args[4]));
+        ps.setInt(6, field.getId());
+        ps.executeUpdate();
+        ps.close();
+        connection.close();
     }
 
     @Override
-    public void save(Field field) {
-
+    public void remove(int id) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + "sportCentre.sqlite");
+        PreparedStatement ps = connection.prepareStatement("DELETE FROM fields WHERE ID = ?");
+        ps.setInt(1, id);
+        ps.executeUpdate();
+        ps.close();
+        connection.close();
     }
 
-    @Override
-    public void modify(Field field, String[] args) {
+    private int getNextId() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + "sportCentre.sqlite");
+        String query = "SELECT MAX(ID) FROM fields";
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet rs = statement.executeQuery();
+        int id; //TODO check if it works
+        if(rs.next()){
+            id = rs.getInt(1) + 1;
+        }else {
+            id=1;
+        }
 
+        rs.close();
+        statement.close();
+        connection.close();
+        return id;
     }
 
-    @Override
-    public void remove(int id) {
-
-    }
 
 
 
--- src/main/java/DomainModel/Field.java
+++ src/main/java/DomainModel/Field.java
@@ -1,17 +1,14 @@
 package main.java.DomainModel;
 
 public class Field {
-    public Field(int ID, String sport, int minimumPeopleRequired, int maximumPeopleRequired, int fineph, boolean availability) {
+    public Field(int ID, String sport, int minimumPeopleRequired, int maximumPeopleRequired, int fineph, boolean availability, SportsCentre centre) {
         this.ID = ID;
         this.sport = sport;
         this.minimumPeopleRequired = minimumPeopleRequired;
         this.maximumPeopleRequired = maximumPeopleRequired;
         this.fineph = fineph;
         this.availability = availability;
-    }
-
-    public Field (int ID){
-        this.ID=ID;
+        this.centre=centre;
     }
 
     private int ID;
@@ -20,7 +17,35 @@ public class Field {
     private int maximumPeopleRequired;
     private int fineph;
 
-
-
+    private SportsCentre centre;
     private boolean availability;
+
+    public String getSport() {
+        return sport;
+    }
+
+    public int getMinimumPeopleRequired() {
+        return minimumPeopleRequired;
+    }
+
+    public int getMaximumPeopleRequired() {
+        return maximumPeopleRequired;
+    }
+
+
+    public int getFineph() {
+        return fineph;
+    }
+
+    public boolean getAvailability() {
+        return availability;
+    }
+
+    public SportsCentre getSportCentre() {
+        return centre;
+    }
+
+    public int getId() {
+        return ID;
+    }
 }
