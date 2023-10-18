e05c422da656321f95c42ca6851586cf2396f83d
Nathan <nathan.acciai@edu.unifi.it>
2023-10-18 18:01:10 +0200

    Booking Dao done first part

--- src/main/java/DAO/BookingDAO.java
+++ src/main/java/DAO/BookingDAO.java
@@ -1,6 +1,66 @@
 package main.java.DAO;
-
+import main.java.DomainModel.Booking;
+import main.java.DomainModel.SportsCentre;
 import main.java.DomainModel.Booking;
 
-public class BookingDAO{
+import java.awt.print.Book;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+public class BookingDAO implements DAO<Booking>{
+    SportsCentreDAO sportsCentreDAO;
+
+    public BookingDAO(SportsCentreDAO sportsCentreDAO ){this.sportsCentreDAO=sportsCentreDAO;}
+
+    @Override
+    public Booking get(int id) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:"+"sportCentre.sqlite");
+        Booking booking= null;
+        PreparedStatement prepStat=connection.prepareStatement("SELECT * FROM bookings WHERE ID=?");
+        prepStat.setInt(1,id);
+        ResultSet resultSet=prepStat.executeQuery();
+        if(resultSet.next()){
+            booking=new Booking(id, LocalDate.parse(resultSet.getString("date")),resultSet.getInt("period"));
+        }
+        resultSet.close();
+        prepStat.close();
+        connection.close();
+        return booking;
+    }
+
+    @Override
+    public ArrayList<Booking> getAll() throws SQLException {
+        Connection connection=DriverManager.getConnection("jdbc:sqlite:"+"sportCentre.sqlite");
+        ArrayList<Booking> bookings= new ArrayList<>();
+        PreparedStatement ps= connection.prepareStatement("SELECT*FROM bookings");
+        ResultSet rs=ps.executeQuery();
+        while(rs.next())
+        {
+            bookings.add(new Booking(rs.getInt("ID"),LocalDate.parse(rs.getString("data")),rs.getInt("period")));
+        }
+        rs.close();
+        ps.close();
+        connection.close();
+        return bookings;
+    }
+
+    @Override
+    public void save(Booking booking) throws SQLException {
+
+    }
+
+    @Override
+    public void modify(Booking booking, String[] args) throws SQLException {
+
+    }
+
+    @Override
+    public void remove(int id) throws SQLException {
+
+    }
+
+    @Override
+    public int getNextId() throws SQLException {
+        return 0;
+    }
 }
--- src/main/java/DomainModel/Booking.java
+++ src/main/java/DomainModel/Booking.java
@@ -1,9 +1,21 @@
 package main.java.DomainModel;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class Booking {
     private int ID;
     private LocalDate date;
     private int period;
+
+    public Booking(int ID, LocalDate date, int period){
+        this.ID=ID;
+        this.date=date;
+        this.period=period;
+    }
+    public int getID(){return ID;}
+    public LocalDate getDate (){return date;}
+    public int getPeriod(){return period;}
+
+
 }
